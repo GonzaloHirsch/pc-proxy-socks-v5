e98553a00a9fff2f6ad878289ec130ecc1708edd
ahenestrosa <ahenestrosa@itba.edu.ar>
2020-06-09 15:41:01 -0300

    [Socks5] Move state definition to .h

--- include/socks5nio.h
+++ include/socks5nio.h
@@ -30,6 +30,107 @@ void socksv5_passive_accept(struct selector_key *key);
 /** Function to free the pool of socks5 instances */
 void socksv5_pool_destroy(void);
 
+/** General state machine */
+enum socks_v5state
+{
+    /**
+     * State when receiving the client initial "hello" and processing
+     * 
+     * Interests:
+     *  - OP_READ -> Reading over the client fd
+     * 
+     * Transitions:
+     *  - HELLO_READ -> While the message is not complete
+     *  - HELLO_WRITE -> When the message is complete
+     *  - ERROR -> In case of an error
+     * */
+    HELLO_READ,
+
+    /**
+     * State when sending the client initial "hello" response
+     * 
+     * Interests:
+     *  - OP_WRITE -> Writing over the client fd
+     * 
+     * Transitions:
+     *  - HELLO_WRITE -> While there are bytes to send
+     *  - REQUEST_READ -> When all the bytes have been transfered
+     *  - ERROR -> In case of an error
+     * */
+    HELLO_WRITE,
+
+    /**
+     * State when receiving the client request
+     * 
+     * Interests:
+     *  - OP_READ -> Reading over the client fd
+     * 
+     * Transitions:
+     *  - REQUEST_READ -> While the message is not complete
+     *  - RESOLVE -> When the message is complete
+     *  - ERROR -> In case of an error
+     * */
+    REQUEST_READ,
+
+    /**
+     * State when the client uses a FQDN to connect to another server, the name must be resolved
+     * 
+     * Interests:
+     *  - OP_READ -> Reading over the resolution response fd
+     *  - OP_WRITE -> Writing to the DNS server the request
+     * 
+     * Transitions:
+     *  - RESOLVE -> While the name is not resolved
+     *  - CONNECTING -> When the name is resolved and the IP address obtained
+     *  - ERROR -> In case of an error
+     * */
+    RESOLVE,
+
+    /**
+     * State when connecting to the server the client request is headed to
+     * 
+     * Interests:
+     * 
+     * Transitions:
+     *  - CONNECTING -> While the server is establishing the connection with the destination of the request
+     *  - REPLY -> When the connection is successful and our server can send the request
+     *  - ERROR -> In case of an error
+     * */
+    CONNECTING,
+
+    /**
+     * State when sending/receiving the request to/from the destination server
+     * 
+     * Interests:
+     *  - OP_READ -> Reading over the server response
+     *  - OP_WRITE -> Writing to server the request
+     * 
+     * Transitions:
+     *  - REPLY -> While the request is being sent and the response is still being copied
+     *  - COPY -> When the server response is finished
+     *  - ERROR -> In case of an error
+     * */
+    REPLY,
+
+    /**
+     * State when copying the response to the client fd
+     * 
+     * Interests:
+     *  - OP_WRITE -> Writing to client fd
+     * 
+     * Transitions:
+     *  - COPY -> While the server is still copying the response
+     *  - DONE -> When the copy process is finished
+     *  - ERROR -> In case of an error
+     * */
+    COPY,
+
+    /** Terminal state */
+    DONE,
+    /** Terminal state */
+    ERROR,
+};
+
 typedef enum AuthOType
 {
     NO_AUTH = 0x00,
@@ -133,5 +234,7 @@ typedef struct socks5
     buffer * readBuffer;
 
 } socks5;
+
+
 
 #endif
--- src/socks5nio.c
+++ src/socks5nio.c
@@ -3,109 +3,9 @@
  * socks5nio.c  - controla el flujo de un proxy SOCKSv5 (sockets no bloqueantes)
  */
 
-/** General state machine */
-enum socks_v5state
-{
-    /**
-     * State when receiving the client initial "hello" and processing
-     * 
-     * Interests:
-     *  - OP_READ -> Reading over the client fd
-     * 
-     * Transitions:
-     *  - HELLO_READ -> While the message is not complete
-     *  - HELLO_WRITE -> When the message is complete
-     *  - ERROR -> In case of an error
-     * */
-    HELLO_READ,
 
-    /**
-     * State when sending the client initial "hello" response
-     * 
-     * Interests:
-     *  - OP_WRITE -> Writing over the client fd
-     * 
-     * Transitions:
-     *  - HELLO_WRITE -> While there are bytes to send
-     *  - REQUEST_READ -> When all the bytes have been transfered
-     *  - ERROR -> In case of an error
-     * */
-    HELLO_WRITE,
 
-    /**
-     * State when receiving the client request
-     * 
-     * Interests:
-     *  - OP_READ -> Reading over the client fd
-     * 
-     * Transitions:
-     *  - REQUEST_READ -> While the message is not complete
-     *  - RESOLVE -> When the message is complete
-     *  - ERROR -> In case of an error
-     * */
-    REQUEST_READ,
 
-    /**
-     * State when the client uses a FQDN to connect to another server, the name must be resolved
-     * 
-     * Interests:
-     *  - OP_READ -> Reading over the resolution response fd
-     *  - OP_WRITE -> Writing to the DNS server the request
-     * 
-     * Transitions:
-     *  - RESOLVE -> While the name is not resolved
-     *  - CONNECTING -> When the name is resolved and the IP address obtained
-     *  - ERROR -> In case of an error
-     * */
-    RESOLVE,
-
-    /**
-     * State when connecting to the server the client request is headed to
-     * 
-     * Interests:
-     * 
-     * Transitions:
-     *  - CONNECTING -> While the server is establishing the connection with the destination of the request
-     *  - REPLY -> When the connection is successful and our server can send the request
-     *  - ERROR -> In case of an error
-     * */
-    CONNECTING,
-
-    /**
-     * State when sending/receiving the request to/from the destination server
-     * 
-     * Interests:
-     *  - OP_READ -> Reading over the server response
-     *  - OP_WRITE -> Writing to server the request
-     * 
-     * Transitions:
-     *  - REPLY -> While the request is being sent and the response is still being copied
-     *  - COPY -> When the server response is finished
-     *  - ERROR -> In case of an error
-     * */
-    REPLY,
-
-    /**
-     * State when copying the response to the client fd
-     * 
-     * Interests:
-     *  - OP_WRITE -> Writing to client fd
-     * 
-     * Transitions:
-     *  - COPY -> While the server is still copying the response
-     *  - DONE -> When the copy process is finished
-     *  - ERROR -> In case of an error
-     * */
-    COPY,
-
-    /** Terminal state */
-    DONE,
-    /** Terminal state */
-    ERROR,
-};
-
-////////////////////////////////////////////////////////////////////
-// Definición de variables para cada estado
 
 // Tabla con informacion de todos los estados.
 static const struct state_definition client_statbl[];
@@ -117,20 +17,6 @@ static const struct state_definition client_statbl[];
  */
 static struct socks5 * socks5_new(const int client){
 
-    /** estados para el client_fd */
-    union {
-        struct hello_st hello;
-        struct request_st request;
-        struct copy copy;
-    } client;
-    /** estados para el origin_fd */
-    union {
-        struct connecting conn;
-        struct copy copy;
-    } orig;
-
-    // TODO: CONTINUE DEFINITON
-};
     // Initialize the Socks5 structure which contain the state machine and other info for the socket.
     struct socks5 * sockState = malloc(sizeof(struct socks5));
     if (sockState == NULL){
