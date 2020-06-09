1aa385a095993cea1e7b40ef31704cb8f0447d3b
Gonzalo Hirsch <ghirsch@itba.edu.ar>
2020-06-09 13:59:08 -0300

    [Socks5] Added signal handling in order to be able to free resources

--- src/socksv5.c
+++ src/socksv5.c
@@ -10,13 +10,29 @@ void slaveSocketHandleWrite(struct selector_key *key);
 void slaveSocketHandleBlock(struct selector_key *key);
 void slaveSocketHandleClose(struct selector_key *key);
 
-void initSocksState(Socks5 * sockState);
-void freeSocksState(Socks5 * sockState);
+void initSocksState(Socks5 *sockState);
+void freeSocksState(Socks5 *sockState);
 
-void renderToState(struct selector_key * key, char * received, int valread);
+void renderToState(struct selector_key *key, char *received, int valread);
+
+static void signal_handler(const int signal);
 
 // Address for socket binding
-struct sockaddr_in * address;
+struct sockaddr_in *address;
+
+// Static boolean to be used as a way to end the server with a signal in a more clean way
+static bool finished = false;
+
+/**
+ * Handler for an interruption signal, just changes the server state to finished
+ * @param signal integer representing the signal sent
+ * 
+ * */
+static void signal_handler(const int signal)
+{
+    printf("Received signal %d, exiting now", signal);
+    finished = true;
+}
 
 int main()
 {
@@ -24,7 +40,7 @@ int main()
     int master_socket;
 
     // Selector for concurrent connexions
-    fd_selector selector = NULL; 
+    fd_selector selector = NULL;
 
     // Setting the socket binding for the server to work ok
     address = malloc(sizeof(address));
@@ -68,6 +84,11 @@ int main()
 
     // TODO: CREATE SCTP SOCKET HERE!
 
+    // ----------------- CREATING SIGNAL HANDLERS -----------------
+
+    // Handling the SIGTERM signal, in order to make server stopping more clean and be able to free resources
+    signal(SIGTERM, signal_handler);
+
     // ----------------- CREATE THE SELECTOR -----------------
 
     // Creating the configuration for the select
@@ -76,106 +97,114 @@ int main()
         .select_timeout = {
             .tv_sec = 10, // Time in seconds
             .tv_nsec = 0  // Time in nanos
-        }
-    };
+        }};
 
     // Initializing the selector using the created configuration
-    if (selector_init(&selector_configuration) == 0){
+    if (selector_init(&selector_configuration) == 0)
+    {
         perror("Initializing the selector");
         exit(EXIT_FAILURE);
     }
 
     // Instancing the selector
     selector = selector_new(SELECTOR_MAX_ELEMENTS);
-    if (selector == NULL){
+    if (selector == NULL)
+    {
         perror("Creating the selector");
         exit(EXIT_FAILURE);
     }
 
     // Create socket handler for the master socket
-    fd_handler * masterSocketHandler = malloc(sizeof(fd_handler));
+    fd_handler *masterSocketHandler = malloc(sizeof(fd_handler));
     masterSocketHandler->handle_read = masterSocketHandle;
     masterSocketHandler->handle_write = NULL;
     masterSocketHandler->handle_block = NULL;
     masterSocketHandler->handle_close = masterSocketHandleClose;
 
-    // Add the master socket to the managed fds.
-    // TODO: Review this
-    selector_status resultStatus;
-    resultStatus = selector_register(selector, master_socket, masterSocketHandler, OP_READ, NULL);
-    if(resultStatus != SELECTOR_SUCCESS){
-        printf("Error in master socket: %s", selector_error(resultStatus));
+    // TODO: Create socket handler for the SCTP socket
+
+    // Register the master socket to the managed fds
+    selector_status ss_master = SELECTOR_SUCCESS;
+    ss_master = selector_register(selector, master_socket, masterSocketHandler, OP_READ, NULL);
+    if (ss_master != SELECTOR_SUCCESS)
+    {
+        printf("Error in master socket: %s", selector_error(ss_master));
     }
 
+    // TODO: Register the SCTP socket to the managed fds
 
     // Accept the incoming connection
     printf("Waiting for connections on socket %d\n", master_socket);
 
-    // Forever cicle until fatal error.
-    while (TRUE)
+    // Cycle until a signal is received as finished
+    while (!finished)
     {
         // Wait for activiy of one of the sockets:
         // -Master Socket --> New connection.
         // -Child Sockets --> Read or write operation
-        resultStatus = selector_select(selector);
-        if(resultStatus != SELECTOR_SUCCESS){
-            printf("Error awaiting for select: %s\n", selector_error(resultStatus));
+        ss_master = selector_select(selector);
+        if (ss_master != SELECTOR_SUCCESS)
+        {
+            printf("Error awaiting for select: %s\n", selector_error(ss_master));
         }
-    
     }
+
+    // TODO: FREE ALL RESOURCES
 
     return 0;
 }
 
-
 /*  Handle for the master socket.
     Used to handle new connections.
 */
-void masterSocketHandle(struct selector_key *key){
+void masterSocketHandle(struct selector_key *key)
+{
 
     int newSocket;
-    socklen_t addrlen = (socklen_t) sizeof(*address);
+    socklen_t addrlen = (socklen_t)sizeof(*address);
 
     // Accpet the new socket.
-    if (newSocket = accept(newSocket, (struct sockaddr *)address, &addrlen) < 0){
-            perror("Error: Error accepting new socket\n");
-            exit(EXIT_FAILURE);
-        }
+    if (newSocket = accept(newSocket, (struct sockaddr *)address, &addrlen) < 0)
+    {
+        perror("Error: Error accepting new socket\n");
+        exit(EXIT_FAILURE);
+    }
 
-        // Inform user of socket number - used in send and receive commands
-        printf("New connection , socket fd is %d , ip is : %s , port : %d \n", newSocket, inet_ntoa(address->sin_addr), ntohs(address->sin_port));
+    // Inform user of socket number - used in send and receive commands
+    printf("New connection , socket fd is %d , ip is : %s , port : %d \n", newSocket, inet_ntoa(address->sin_addr), ntohs(address->sin_port));
 
-        // Handler for the slave sockets.
-        // Create socket handler for the master socket
-        fd_handler * slaveSocketHandler = malloc(sizeof(fd_handler));
-        slaveSocketHandler->handle_read = slaveSocketHandleRead;
-        slaveSocketHandler->handle_write = slaveSocketHandleWrite;
-        slaveSocketHandler->handle_block = slaveSocketHandleBlock;
-        slaveSocketHandler->handle_close = masterSocketHandleClose;
+    // Handler for the slave sockets.
+    // Create socket handler for the master socket
+    fd_handler *slaveSocketHandler = malloc(sizeof(fd_handler));
+    slaveSocketHandler->handle_read = slaveSocketHandleRead;
+    slaveSocketHandler->handle_write = slaveSocketHandleWrite;
+    slaveSocketHandler->handle_block = slaveSocketHandleBlock;
+    slaveSocketHandler->handle_close = masterSocketHandleClose;
 
-        // Initialize the Socks5 structure which contain the state machine and other info for the socket.
-        Socks5 * sockState = malloc(sizeof(Socks5));
-        initSocksState(sockState);
+    // Initialize the Socks5 structure which contain the state machine and other info for the socket.
+    Socks5 *sockState = malloc(sizeof(Socks5));
+    initSocksState(sockState);
 
-        // Register the new file descriptor.
-        selector_status resultStatus;
-        resultStatus = selector_register(key->s, newSocket, slaveSocketHandler, OP_READ + OP_WRITE, (void *) sockState);
-        if(resultStatus != SELECTOR_SUCCESS){
-            printf("Error: %s while creating registering new fd", selector_error(resultStatus));
-        }
+    // Register the new file descriptor.
+    selector_status resultStatus;
+    resultStatus = selector_register(key->s, newSocket, slaveSocketHandler, OP_READ + OP_WRITE, (void *)sockState);
+    if (resultStatus != SELECTOR_SUCCESS)
+    {
+        printf("Error: %s while creating registering new fd", selector_error(resultStatus));
+    }
 }
 
 // TODO: implement
-void masterSocketHandleClose(struct selector_key *key){
-
+void masterSocketHandleClose(struct selector_key *key)
+{
 }
 
-
-void slaveSocketHandleRead(struct selector_key *key){
+void slaveSocketHandleRead(struct selector_key *key)
+{
 
     int sd = key->fd, valread;
     char received[BUFFERSIZE + 1];
-    socklen_t addrlen = (socklen_t) sizeof(*address);
+    socklen_t addrlen = (socklen_t)sizeof(*address);
 
     //Check if it was for closing , and also read the incoming message
     if ((valread = read(sd, received, BUFFERSIZE)) <= 0)
@@ -183,10 +212,10 @@ void slaveSocketHandleRead(struct selector_key *key){
         // Remove from selector.
         // This will call slaveSocketHandleClose --> All cleaning ops go there.
         selector_status responseStatus = selector_unregister_fd(key->s, key->fd);
-        if(responseStatus != SELECTOR_SUCCESS){
+        if (responseStatus != SELECTOR_SUCCESS)
+        {
             printf("Error: %s while processing read\n", selector_error(responseStatus));
         }
-
     }
     else
     {
@@ -195,15 +224,15 @@ void slaveSocketHandleRead(struct selector_key *key){
         //TODO: To implement shortly
         //render_to_state();
     }
-
 }
 
 /*  Handle for write of slave socket.
 */
-void slaveSocketHandleWrite(struct selector_key *key){
+void slaveSocketHandleWrite(struct selector_key *key)
+{
 
-    Socks5 * sockState = (Socks5 *)key->data;
-    buffer * writeBuffer = (buffer *)sockState->writeBuffer;
+    Socks5 *sockState = (Socks5 *)key->data;
+    buffer *writeBuffer = (buffer *)sockState->writeBuffer;
     int sd = key->fd;
 
     size_t bytesToSend = writeBuffer->read - writeBuffer->write;
@@ -225,7 +254,8 @@ void slaveSocketHandleWrite(struct selector_key *key){
 
             // Si se pudieron mandar todos los bytes limpiamos el buffer
             // y sacamos el fd para el select
-            if (bytesLeft == 0){
+            if (bytesLeft == 0)
+            {
                 buffer_reset(writeBuffer);
             }
             else
@@ -238,31 +268,33 @@ void slaveSocketHandleWrite(struct selector_key *key){
 }
 
 // TODO: Implement
-void slaveSocketHandleBlock(struct selector_key *key){
-
+void slaveSocketHandleBlock(struct selector_key *key)
+{
 }
 
 //TODO: Implement
-void slaveSocketHandleClose(struct selector_key *key){
-
+void slaveSocketHandleClose(struct selector_key *key)
+{
 }
 
-void initSocksState(Socks5 * sockState){
-    if (sockState == NULL){
+void initSocksState(Socks5 *sockState)
+{
+    if (sockState == NULL)
+    {
         perror("Error: Initizalizing null Socks5\n");
     }
 
     stm_init(&(sockState->stm));
 
-     // Write Buffer for the socket(Initialized)
-    buffer * writeBuffer = malloc(sizeof(buffer));
-    buffer_init(writeBuffer, BUFFERSIZE+1, malloc(BUFFERSIZE+1));
+    // Write Buffer for the socket(Initialized)
+    buffer *writeBuffer = malloc(sizeof(buffer));
+    buffer_init(writeBuffer, BUFFERSIZE + 1, malloc(BUFFERSIZE + 1));
     sockState->writeBuffer = writeBuffer;
 }
 
 // Todo: Implement
-void freeSocksState(Socks5 * sockState){
-    
+void freeSocksState(Socks5 *sockState)
+{
 }
 
 /*
