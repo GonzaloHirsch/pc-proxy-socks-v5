20f1916f7492d2681a916bae0b0481c15221b993
francisco choi <fchoi@itba.edu.ar>
2020-06-11 01:34:10 -0300

    packet fill

---
+++ dnsPacket.h
@@ -0,0 +1,142 @@
+
+//Header Files
+#include<stdio.h> //printf
+#include<string.h>    //strlen
+#include<stdlib.h>    //malloc
+#include<sys/socket.h>    //you know what this is for
+#include<arpa/inet.h> //inet_addr , inet_ntoa , ntohs etc
+#include<netinet/in.h>
+#include<unistd.h>    //getpid
+
+
+#define T_A 1 //Ipv4 address
+#define T_NS 2 //Nameserver
+#define T_CNAME 5 // canonical name
+#define T_SOA 6 /* start of authority zone */
+#define T_PTR 12 /* domain name pointer */
+#define T_MX 15 //Mail server
+
+//Constant sized fields of query structure
+struct QUESTION
+{
+    unsigned short qtype;
+    unsigned short qclass;
+};
+ 
+//Constant sized fields of the resource record structure
+#pragma pack(push, 1)
+struct R_DATA
+{
+    unsigned short type;
+    unsigned short _class;
+    unsigned int ttl;
+    unsigned short data_len;
+};
+#pragma pack(pop)
+ 
+//Pointers to resource record contents
+struct RES_RECORD
+{
+    unsigned char *name;
+    struct R_DATA *resource;
+    unsigned char *rdata;
+};
+ 
+//Structure of a Query
+typedef struct
+{
+    unsigned char *name;
+    struct QUESTION *ques;
+} QUERY;
+ 
+
+
+struct DNS_HEADER
+{
+    unsigned short id; // identification number
+ 
+    unsigned char rd :1; // recursion desired
+    unsigned char tc :1; // truncated message
+    unsigned char aa :1; // authoritive answer
+    unsigned char opcode :4; // purpose of message
+    unsigned char qr :1; // query/response flag
+ 
+    unsigned char rcode :4; // response code
+    unsigned char cd :1; // checking disabled
+    unsigned char ad :1; // authenticated data
+    unsigned char z :1; // its z! reserved
+    unsigned char ra :1; // recursion available
+ 
+    unsigned short q_count; // number of question entries
+    unsigned short ans_count; // number of answer entries
+    unsigned short auth_count; // number of authority entries
+    unsigned short add_count; // number of resource entries
+};
+
+void host_dns_format(char * dns, char * host);
+
+
+char * generate_dns_req(char * host){
+
+    char buf[65536],*qname;
+
+    struct DNS_HEADER *dns = NULL;
+    struct QUESTION *qinfo = NULL;
+
+
+    dns = (struct DNS_HEADER *)&buf; //overlaps header into the buffer
+
+
+    dns->id = (unsigned short) htons(getpid()); //number to identify dns request
+    dns->qr = 0; //This is a query
+    dns->opcode = 0; //This is a standard query
+    dns->aa = 0; //Not Authoritative
+    dns->tc = 0; //This message is not truncated
+    dns->rd = 1; //Recursion Desired
+    dns->ra = 0; //Recursion not available! hey we dont have it (lol)
+    dns->z = 0;
+    dns->ad = 0;
+    dns->cd = 0;
+    dns->rcode = 0;
+    dns->q_count = htons(1); //we have only 1 question
+    dns->ans_count = 0;
+    dns->auth_count = 0;
+    dns->add_count = 0;
+
+
+    qname = (char*) (buf + sizeof(struct DNS_HEADER));
+
+    host_dns_format(qname , host);
+    qinfo =(struct QUESTION*)&buf[sizeof(struct DNS_HEADER) + (strlen((const char*)qname) + 1)]; //fill it
+ 
+    qinfo->qtype = htons(T_A); //type of the query , A , MX , CNAME , NS etc
+    qinfo->qclass = htons(1); //internet 
+
+    return buf;
+
+
+}
+
+//if it is www.google.com it converts to www3google6com3
+
+ void host_dns_format(char * dns, char * host){
+
+    int lock = 0 , i;
+    strcat((char*)host,".");
+     
+    for(i = 0 ; i < strlen((char*)host) ; i++) 
+    {
+        if(host[i]=='.') 
+        {
+            *dns++ = i-lock;
+            for(;lock<i;lock++) 
+            {
+                *dns++=host[lock];
+            }
+            lock++; //or lock=i+1;
+        }
+    }
+    *dns++='\0';
+}
+
+
