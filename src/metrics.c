14b5238c683bfed0bb44dba892d27bfbee2531a0
Gonzalo Hirsch <ghirsch@itba.edu.ar>
2020-06-12 20:34:57 -0300

    [Metrics] Added metrics interface

---
+++ src/metrics.c
@@ -0,0 +1,95 @@
+#include "metrics.h"
+
+static metrics m;
+
+/**
+ * Method to init the metrics pointer
+*/
+void init_metrics()
+{
+    m = malloc(sizeof(metrics));
+}
+
+void add_transfered_bytes(uint8_t n)
+{
+    m->transfered_bytes += n;
+}
+
+void add_historic_connections(uint8_t n)
+{
+    m->historic_connections += n;
+}
+
+void add_current_connections(uint8_t n)
+{
+    m->current_connections += n;
+}
+
+void remove_current_connections(uint8_t n)
+{
+    m->current_connections -= n;
+}
+
+void add_resolved_addresses(uint8_t n)
+{
+    m->resolved_addresses += n;
+}
+
+void add_given_ips(uint8_t n)
+{
+    m->given_ips += n;
+}
+
+void add_ipv6(uint8_t n)
+{
+    m->ipv6_count += n;
+}
+
+void add_ipv4(uint8_t n)
+{
+    m->ipv4_count += n;
+}
+
+void add_login(uint8_t n)
+{
+    m->log_in_count += n;
+}
+
+typedef struct metric
+{
+    uint8_t transfered_bytes;
+    uint8_t historic_connections;
+    uint8_t current_connections;
+    uint8_t resolved_addresses;
+    uint8_t given_ips;
+    uint8_t ipv6_count;
+    uint8_t ipv4_count;
+    uint8_t log_in_count;
+} metric;
+
+char *get_metrics_for_output()
+{
+    if (m == NULL)
+    {
+        return NULL;
+    }
+
+    // Estimate on the size of the response
+    char result[320];
+
+    // Format to be used
+    char *fmt = "transfered_bytes:%d, historic_connections:%d, current_connections:%d, resolved_addresses:%d, given_ips:%d, ipv6_count:%d, ipv4_count:%d, log_in_count:%d";
+
+    // Printing to the result buffer
+    sprintf(result, fmt, m->transfered_bytes, m->historic_connections, m->current_connections, m->resolved_addresses, m->given_ips, m->ipv6_count, m->ipv4_count, m->log_in_count);
+
+    return result;
+}
+
+/**
+ * Method to destroy the metrics pointer
+*/
+void destroy_metrics()
+{
+    free(m);
+}
